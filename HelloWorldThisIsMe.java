b548111683c378a58577343bb4b4a3c128157665
Jaxyn Gaillard <jaxyn.gaillard@tooeleschools.org>
2020-09-10 09:46:36 -0600

    install activity assignment 9/10/2022

---
+++ HelloWorldThisIsMe.java
@@ -0,0 +1,16 @@
+class HelloWorldThisIsMe{
+
+public static void main(String[] args){
+        System.out.println("Hello World my name is Jaxyn Gailard"); 
+        System.out.println("I am 16 years old and I turn 17 on May 22 2021");
+        System.out.println("I have a dog and a cat, aswell as a turtle");
+        System.out.println("I enjoy composing music in my alone time");
+        System.out.println("I am even working tward getting a job");
+        System.out.println("Otherwise I enjoy playing games");
+        System.out.println("Some of the games I play are gmod, Half life series, Team fortress 2, and more");
+        System.out.println("I also enjoy taking camping");
+        System.out.println("I always say if I could stay in the mountains forever I would");
+        System.out.println("I usally spend my time making fires or building shelters");
+
+}
+} 
